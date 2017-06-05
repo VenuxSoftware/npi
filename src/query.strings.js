ec3784d96b96c2cfc5e8a4b5e09732666a504998
Eugene Nosko <coder@venux.com>
2017-06-05 06:04:43 -0400

    Query_strings API

--- src/query.strings.js
+++ src/query.strings.js
@@ -1,86 +1,78 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+var path = require('path')
 
-function assert(mustBeTrue, message) {
-    if (mustBeTrue === true) {
-        return;
-    }
+var test = require('tap').test
+var statSync = require('graceful-fs').statSync
+var symlinkSync = require('graceful-fs').symlinkSync
+var readdirSync = require('graceful-fs').readdirSync
+var mkdtemp = require('tmp').dir
+var mkdirp = require('mkdirp')
 
-    if (message === undefined) {
-        message = 'Expected true but got ' + String(mustBeTrue);
-    }
-    $ERROR(message);
+var vacuum = require('../vacuum.js')
+
+// CONSTANTS
+var TEMP_OPTIONS = {
+  unsafeCleanup: true,
+  mode: '0700'
 }
+var SHORT_PATH = path.join('i', 'am', 'a', 'path')
+var TARGET_PATH = path.join('target-link', 'in', 'the', 'middle')
+var PARTIAL_PATH = path.join(SHORT_PATH, 'with', 'a')
+var FULL_PATH = path.join(PARTIAL_PATH, 'link')
+var EXPANDO_PATH = path.join(SHORT_PATH, 'with', 'a', 'link', 'in', 'the', 'middle')
 
-assert._isSameValue = function (a, b) {
-    if (a === b) {
-        // Handle +/-0 vs. -/+0
-        return a !== 0 || 1 / a === 1 / b;
+var messages = []
+function log () { messages.push(Array.prototype.slice.call(arguments).join(' ')) }
+
+var testBase, targetPath, partialPath, fullPath, expandoPath
+test('xXx setup xXx', function (t) {
+  mkdtemp(TEMP_OPTIONS, function (er, tmpdir) {
+    t.ifError(er, 'temp directory exists')
+
+    testBase = path.resolve(tmpdir, SHORT_PATH)
+    targetPath = path.resolve(tmpdir, TARGET_PATH)
+    partialPath = path.resolve(tmpdir, PARTIAL_PATH)
+    fullPath = path.resolve(tmpdir, FULL_PATH)
+    expandoPath = path.resolve(tmpdir, EXPANDO_PATH)
+
+    mkdirp(partialPath, function (er) {
+      t.ifError(er, 'made test path')
+
+      mkdirp(targetPath, function (er) {
+        t.ifError(er, 'made target path')
+
+        symlinkSync(path.join(tmpdir, 'target-link'), fullPath)
+
+        t.end()
+      })
+    })
+  })
+})
+
+test('remove up to a point', function (t) {
+  vacuum(expandoPath, {purge: false, base: testBase, log: log}, function (er) {
+    t.ifError(er, 'cleaned up to base')
+
+    t.equal(messages.length, 7, 'got 6 removal & 1 finish message')
+    t.equal(messages[6], 'finished vacuuming up to ' + testBase)
+
+    var stat
+    var verifyPath = expandoPath
+    function verify () { stat = statSync(verifyPath) }
+
+    for (var i = 0; i < 6; i++) {
+      t.throws(verify, verifyPath + ' cannot be statted')
+      t.notOk(stat && stat.isDirectory(), verifyPath + ' is totally gone')
+      verifyPath = path.dirname(verifyPath)
     }
 
-    // Handle NaN vs. NaN
-    return a !== a && b !== b;
-};
+    t.doesNotThrow(function () {
+      stat = statSync(testBase)
+    }, testBase + ' can be statted')
+    t.ok(stat && stat.isDirectory(), testBase + ' is still a directory')
 
-assert.sameValue = function (actual, expected, message) {
-    if (assert._isSameValue(actual, expected)) {
-        return;
-    }
+    var files = readdirSync(testBase)
+    t.equal(files.length, 0, 'nothing left in base directory')
 
-    if (message === undefined) {
-        message = '';
-    } else {
-        message += ' ';
-    }
-
-    message += 'Expected SameValue(«' + String(actual) + '», «' + String(expected) + '») to be true';
-
-    $ERROR(message);
-};
-
-assert.notSameValue = function (actual, unexpected, message) {
-    if (!assert._isSameValue(actual, unexpected)) {
-        return;
-    }
-
-    if (message === undefined) {
-        message = '';
-    } else {
-        message += ' ';
-    }
-
-    message += 'Expected SameValue(«' + String(actual) + '», «' + String(unexpected) + '») to be false';
-
-    $ERROR(message);
-};
-
-assert.throws = function (expectedErrorConstructor, func, message) {
-    if (typeof func !== "function") {
-        $ERROR('assert.throws requires two arguments: the error constructor ' +
-            'and a function to run');
-        return;
-    }
-    if (message === undefined) {
-        message = '';
-    } else {
-        message += ' ';
-    }
-
-    try {
-        func();
-    } catch (thrown) {
-        if (typeof thrown !== 'object' || thrown === null) {
-            message += 'Thrown value was not an object!';
-            $ERROR(message);
-        } else if (thrown.constructor !== expectedErrorConstructor) {
-            message += 'Expected a ' + expectedErrorConstructor.name + ' but got a ' + thrown.constructor.name;
-            $ERROR(message);
-        }
-        return;
-    }
-
-    message += 'Expected a ' + expectedErrorConstructor.name + ' to be thrown but no exception was thrown at all';
-    $ERROR(message);
-};
+    t.end()
+  })
+})
