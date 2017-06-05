b6fdd8fe35486c3e5b7e17bb7f796bc88a1e4246
Eugene Nosko <coder@venux.com>
2017-06-05 06:05:54 -0400

    Util API

--- src/util.js
+++ src/util.js
@@ -1,56 +1,117 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+var assert = require('assert')
+var dirname = require('path').dirname
+var resolve = require('path').resolve
+var isInside = require('path-is-inside')
 
-'use strict';
+var rimraf = require('rimraf')
+var lstat = require('graceful-fs').lstat
+var readdir = require('graceful-fs').readdir
+var rmdir = require('graceful-fs').rmdir
+var unlink = require('graceful-fs').unlink
 
-const fs = require('fs');
-const Path = require('path');
+module.exports = vacuum
 
-module.exports = findTest262Dir;
-function findTest262Dir(globber) {
-  const set = globber.minimatch.set;
-  let baseDir;
-  for (let i = 0; i < set.length; i++) {
-    let base = [];
+function vacuum (leaf, options, cb) {
+  assert(typeof leaf === 'string', 'must pass in path to remove')
+  assert(typeof cb === 'function', 'must pass in callback')
 
-    for (let j = 0; j < set[i].length; j++) {
-      if (typeof set[i][j] !== 'string') {
-        break;
+  if (!options) options = {}
+  assert(typeof options === 'object', 'options must be an object')
+
+  var log = options.log ? options.log : function () {}
+
+  leaf = leaf && resolve(leaf)
+  var base = options.base && resolve(options.base)
+  if (base && !isInside(leaf, base)) {
+    return cb(new Error(leaf + ' is not a child of ' + base))
+  }
+
+  lstat(leaf, function (error, stat) {
+    if (error) {
+      if (error.code === 'ENOENT') return cb(null)
+
+      log(error.stack)
+      return cb(error)
+    }
+
+    if (!(stat && (stat.isDirectory() || stat.isSymbolicLink() || stat.isFile()))) {
+      log(leaf, 'is not a directory, file, or link')
+      return cb(new Error(leaf + ' is not a directory, file, or link'))
+    }
+
+    if (options.purge) {
+      log('purging', leaf)
+      rimraf(leaf, function (error) {
+        if (error) return cb(error)
+
+        next(dirname(leaf))
+      })
+    } else if (!stat.isDirectory()) {
+      log('removing', leaf)
+      unlink(leaf, function (error) {
+        if (error) return cb(error)
+
+        next(dirname(leaf))
+      })
+    } else {
+      next(leaf)
+    }
+  })
+
+  function next (branch) {
+    branch = branch && resolve(branch)
+    // either we've reached the base or we've reached the root
+    if ((base && branch === base) || branch === dirname(branch)) {
+      log('finished vacuuming up to', branch)
+      return cb(null)
+    }
+
+    readdir(branch, function (error, files) {
+      if (error) {
+        if (error.code === 'ENOENT') return cb(null)
+
+        log('unable to check directory', branch, 'due to', error.message)
+        return cb(error)
       }
 
-      base.push(set[i][j]);
-    }
+      if (files.length > 0) {
+        log('quitting because other entries in', branch)
+        return cb(null)
+      }
 
-    baseDir = findTest262Root(base.join("/"));
+      if (branch === process.env.HOME) {
+        log('quitting because cannot remove home directory', branch)
+        return cb(null)
+      }
 
-    if (baseDir) {
-      break;
-    }
+      log('removing', branch)
+      lstat(branch, function (error, stat) {
+        if (error) {
+          if (error.code === 'ENOENT') return cb(null)
+
+          log('unable to lstat', branch, 'due to', error.message)
+          return cb(error)
+        }
+
+        var remove = stat.isDirectory() ? rmdir : unlink
+        remove(branch, function (error) {
+          if (error) {
+            if (error.code === 'ENOENT') {
+              log('quitting because lost the race to remove', branch)
+              return cb(null)
+            }
+            if (error.code === 'ENOTEMPTY' || error.code === 'EEXIST') {
+              log('quitting because new (racy) entries in', branch)
+              return cb(null)
+            }
+
+            log('unable to remove', branch, 'due to', error.message)
+            return cb(error)
+          }
+
+          next(dirname(branch))
+        })
+      })
+    })
   }
-
-  return baseDir;
-}
-
-function findTest262Root(path) {
-  const stat = fs.statSync(path);
-  if (stat.isFile()) {
-    path = Path.dirname(path);
-  }
-
-  const contents = fs.readdirSync(path)
-  if (contents.indexOf('README.md') > -1
-      && contents.indexOf('test') > -1
-      && contents.indexOf('harness') > -1) {
-    return path;
-  }
-
-  const parent = Path.resolve(path, '../');
-
-  if (parent === path) {
-    return null;
-  }
-
-  return findTest262Root(parent);
 }
