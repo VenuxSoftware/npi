e69fdab05786348050ed6e2ea574e01dca80e2f6
Eugene Nosko <coder@venux.com>
2018-01-12 04:38:36 -0500

    Path

--- src/path.js
+++ src/path.js
@@ -1,16 +1,31 @@
-var test = require('tap').test
+var baseCopy = require('../internal/baseCopy'),
+    keysIn = require('../object/keysIn');
 
-var vacuum = require('../vacuum.js')
+/**
+ * Converts `value` to a plain object flattening inherited enumerable
+ * properties of `value` to own properties of the plain object.
+ *
+ * @static
+ * @memberOf _
+ * @category Lang
+ * @param {*} value The value to convert.
+ * @returns {Object} Returns the converted plain object.
+ * @example
+ *
+ * function Foo() {
+ *   this.b = 2;
+ * }
+ *
+ * Foo.prototype.c = 3;
+ *
+ * _.assign({ 'a': 1 }, new Foo);
+ * // => { 'a': 1, 'b': 2 }
+ *
+ * _.assign({ 'a': 1 }, _.toPlainObject(new Foo));
+ * // => { 'a': 1, 'b': 2, 'c': 3 }
+ */
+function toPlainObject(value) {
+  return baseCopy(value, keysIn(value));
+}
 
-test('vacuum errors when base is set and path is not under it', function (t) {
-  vacuum('/a/made/up/path', {base: '/root/elsewhere'}, function (er) {
-    t.ok(er, 'got an error')
-    t.equal(
-      er.message,
-      '/a/made/up/path is not a child of /root/elsewhere',
-      'got the expected error message'
-    )
-
-    t.end()
-  })
-})
+module.exports = toPlainObject;
