e66e90d229efc642d881a9610806f857e8653d88
Eugene Nosko <coder@venux.com>
2018-01-12 04:47:38 -0500

    Strings decoder

--- src/strings.decoder.js
+++ src/strings.decoder.js
@@ -1,46 +1,57 @@
+var fs = require('fs')
 var path = require('path')
+var getUid = require('uid-number')
+var chain = require('slide').chain
+var log = require('npmlog')
+var npm = require('../npm.js')
+var fileCompletion = require('../utils/completion/file-completion.js')
 
-var test = require('tap').test
-var mkdtemp = require('tmp').dir
-var mkdirp = require('mkdirp')
-
-var vacuum = require('../vacuum.js')
-
-// CONSTANTS
-var TEMP_OPTIONS = {
-  unsafeCleanup: true,
-  mode: '0700'
-}
-
-var BASE_PATH = path.join('foo')
-var HOME_PATH = path.join(BASE_PATH, 'foo', 'bar')
-
-var messages = []
-function log () { messages.push(Array.prototype.slice.call(arguments).join(' ')) }
-
-var homePath, basePath, realHome
-test('xXx setup xXx', function (t) {
-  mkdtemp(TEMP_OPTIONS, function (er, tmpdir) {
-    t.ifError(er, 'temp directory exists')
-
-    homePath = path.resolve(tmpdir, HOME_PATH)
-    basePath = path.resolve(tmpdir, BASE_PATH)
-
-    realHome = process.env.HOME
-    process.env.HOME = homePath
-
-    mkdirp(homePath, function (er) {
-      t.ifError(er, 'made test path')
-      t.end()
+function checkFilesPermission (root, fmask, dmask, cb) {
+  if (process.platform === 'win32') return cb(null, true)
+  getUid(npm.config.get('user'), npm.config.get('group'), function (e, uid, gid) {
+    var tracker = log.newItem('checkFilePermissions', 1)
+    if (e) {
+      tracker.finish()
+      tracker.warn('checkFilePermissions', 'Error looking up user and group:', e)
+      return cb(e)
+    }
+    tracker.info('checkFilePermissions', 'Building file list of ' + root)
+    fileCompletion(root, '.', Infinity, function (e, files) {
+      if (e) {
+        tracker.warn('checkFilePermissions', 'Error building file list:', e)
+        tracker.finish()
+        return cb(e)
+      }
+      tracker.addWork(files.length)
+      tracker.completeWork(1)
+      chain(files.map(andCheckFile), function (er) {
+        tracker.finish()
+        cb(null, !er)
+      })
+      function andCheckFile (f) {
+        return [checkFile, f]
+      }
+      function checkFile (f, next) {
+        var file = path.join(root, f)
+        tracker.silly('checkFilePermissions', f)
+        fs.lstat(file, function (e, stat) {
+          tracker.completeWork(1)
+          if (e) return next(e)
+          if (!stat.isDirectory() && !stat.isFile()) return next()
+          // 6 = fs.constants.R_OK | fs.constants.W_OK
+          // constants aren't available on v4
+          fs.access(file, stat.isFile() ? fmask : dmask, (err) => {
+            if (err) {
+              tracker.error('checkFilePermissions', `Missing permissions on ${file}`)
+              return next(new Error('Missing permissions for ' + file))
+            } else {
+              return next()
+            }
+          })
+        })
+      }
     })
   })
-})
+}
 
-test('do not remove home directory', function (t) {
-  vacuum(homePath, {purge: false, base: basePath, log: log}, function (er) {
-    t.ifError(er, 'cleaned up to base')
-    t.equal(messages[0], 'quitting because cannot remove home directory ' + homePath)
-    process.env.HOME = realHome
-    t.end()
-  })
-})
+module.exports = checkFilesPermission
